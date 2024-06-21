68b383d098379211f51b17b6aba1feb2972e822c
TakshaSharma <taksha.sharma8@gmail.com>
2024-06-21 21:12:03 +0530

    home feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Feature from './Components/Feature'
+import  Workflow  from './Components/Workflow'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+      <Hero/>
+        </div>
+        <Feature/>
+       <Workflow/>
     </div>
   )
 }
---
+++ src/Components/Workflow.jsx
@@ -0,0 +1,17 @@
+import { CheckCircle2 } from 'lucide-react'
+import codeImg from ".../assets/code"
+
+import React from 'react'
+
+function Workflow() {
+  return (
+    <div className='mt-20'>
+      <h2 className=''>
+        Accelerate your{" "}</h2>
+        <span className=''></span>
+    </div>
+  )
+}
+
+export default Workflow
+
